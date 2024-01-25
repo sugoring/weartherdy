d092e3b11d239f853a7c8e0e894d02cc7ea7965b
yuni <hys990730@gmail.com>
2024-01-25 23:52:41 +1000

    build api 받아 오기 위한 빌드 설정
    build 및 manifest 수정

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -3,3 +3,5 @@
 	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
 D
 app/build.gradle.kts,d\2\d2a4383452aad84a18429a5844d31330a012a9e6
+P
+ app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,9 @@ android {
 }
 
 dependencies {
+    implementation("com.loopj.android:android-async-http:1.4.11")
+    implementation("com.squareup.retrofit2:retrofit:2.8.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
